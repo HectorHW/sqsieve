8c753d66bc48391889c38be72734628ae2ca6bc7
Vladimir Redkin <vsredkin01@gmail.com>
2022-11-08 19:11:18 +0300

    exhaustive search of solutions

--- src/factor_building.rs
+++ src/factor_building.rs
@@ -3,6 +3,7 @@ use std::iter::repeat_with;
 
 use crate::number_type::{NumberOps, NumberType};
 use crate::{sieve::SmoothNumber, solver::Solution};
+use lazy_static::lazy_static;
 use num_bigint::BigUint;
 use num_traits::FromPrimitive;
 use rand::{thread_rng, Rng};
@@ -29,6 +30,55 @@ fn test_factorization(n: &BigUint, a: &BigUint, b: &BigUint) -> Option<(BigUint,
         return Some((gcd, other_term));
     }
     None
+}
+
+fn is_zero(vec: &[bool]) -> bool {
+    vec.iter().all(|&item| !item)
+}
+
+#[inline]
+fn increase(vec: &mut [bool]) {
+    let mut carry = true;
+    for digit in vec {
+        let new_carry = *digit && carry;
+        *digit = *digit != carry;
+        carry = new_carry;
+    }
+}
+
+lazy_static! {
+    static ref ONE: BigUint = BigUint::from_i32(1).unwrap();
+}
+
+#[inline]
+fn attempt_factorization(
+    n: &BigUint,
+    smoothies: &[SmoothNumber<NumberType>],
+    solution: &Solution,
+    substitution_vector: &[bool],
+) -> Option<(BigUint, BigUint)> {
+    let inclusion = solution.subsitute(substitution_vector, false);
+
+    let (mut a, mut b) = smoothies
+        .iter()
+        .zip(inclusion.iter())
+        .filter_map(|(a, &b)| if b { Some(a) } else { None })
+        .fold((ONE.clone(), ONE.clone()), |acc, item| {
+            let left = acc.0 * &item.number.to_varsize();
+            let right = acc.1
+                * item
+                    .divisors
+                    .iter()
+                    .map(|&(divisor, power)| BigUint::from(divisor).pow(power as u32))
+                    .product::<BigUint>();
+            (left, right)
+        });
+
+    if b > a {
+        std::mem::swap(&mut a, &mut b);
+    }
+
+    test_factorization(n, &a, &b)
 }
 
 pub fn find_factor(
@@ -63,65 +113,52 @@ pub fn find_factor(
 
     // if it fails, it means that all lonely numbers produce trivial factorization, do not include them
 
-    let attempts = 2usize
-        .checked_pow(solution.free_variables.len() as u32)
-        .map(|v| v.min(10_000))
-        .unwrap_or(10_000);
-
-    let mut rng = thread_rng();
-
-    let one = BigUint::from_i32(1).unwrap();
-
     let limit = solution.free_variables.len();
 
-    println!("limit: {limit}");
-
-    let mut current_limit = limit;
-
-    let pressure_variants = repeat_with(|| {
-        let next_value = current_limit;
-        current_limit /= 2;
-        next_value as u32
-    })
-    .take_while(|&n| n >= 2);
+    println!("variables for exhaustive search: {limit}");
 
     let mut free_mapping = vec![false; solution.free_variables.len()];
 
-    for pressure in pressure_variants {
-        println!("trying 1/{}", pressure);
-        for _attempt in 0..attempts {
-            free_mapping
-                .iter_mut()
-                .for_each(|position| *position = rng.gen_ratio(1, pressure));
+    increase(&mut free_mapping);
 
-            let inclusion = solution.subsitute(&free_mapping, false);
-
-            let (mut a, mut b) = smoothies
-                .iter()
-                .zip(inclusion.iter())
-                .filter_map(|(a, &b)| if b { Some(a) } else { None })
-                .fold((one.clone(), one.clone()), |acc, item| {
-                    let left = acc.0 * &item.number.to_varsize();
-                    let right = acc.1
-                        * item
-                            .divisors
-                            .iter()
-                            .map(|&(divisor, power)| BigUint::from(divisor).pow(power as u32))
-                            .product::<BigUint>();
-                    (left, right)
-                });
-
-            if b > a {
-                std::mem::swap(&mut a, &mut b);
-            }
-
-            let Some(solution) = test_factorization(&n, &a, &b) else {
-                continue;
-            };
-
-            return Some(solution);
+    while !is_zero(&free_mapping) {
+        if let Some(sol) = attempt_factorization(&n, smoothies, solution, &free_mapping) {
+            return Some(sol);
         }
+        increase(&mut free_mapping);
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{increase, is_zero};
+
+    #[test]
+    fn should_turn_0_into_1() {
+        let mut v = vec![false, false, false];
+        increase(&mut v);
+        assert_eq!(v, vec![true, false, false]);
+    }
+
+    #[test]
+    fn should_apply_carry_bit() {
+        let mut v = vec![true, true, true, false];
+        increase(&mut v);
+        assert_eq!(v, vec![false, false, false, true]);
+    }
+
+    #[test]
+    fn should_increase() {
+        let mut v = vec![true, false, true];
+        increase(&mut v);
+        assert_eq!(v, vec![false, true, true]);
+    }
+
+    #[test]
+    fn should_be_zero() {
+        let v = vec![false, false, false];
+        assert!(is_zero(&v))
+    }
 }
