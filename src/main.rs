118ba448e4562fe269742d211e85a92de700c158
Vladimir Redkin <vsredkin01@gmail.com>
2022-11-09 19:27:45 +0300

    multiple attempts to factorize and sieve search stub

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::{
     factor_building::{find_factor_exhaustive, find_factor_simple, find_factors_random},
     number_type::NumberType,
     numbers::{build_factor_base, small_eratosphenes},
-    sieve::{compute_b_limit, sieve_for_smoothies},
+    sieve::{block_division_sieve, compute_b_limit, test_division_sieve},
     solver::{produce_solution, CongruenceSystem},
 };
 
@@ -82,38 +82,73 @@ fn run_factor(n: &NumberType, prime_bound: usize) -> Option<(BigUint, BigUint)> 
     #[cfg(feature = "verbose")]
     println!("factor base: {:?}", factor_base);
 
-    let sieving_limit = (factor_base.len() as f64 * 1.05f64).ceil() as usize;
+    let mut ratio = 1.05;
 
-    println!("need about {sieving_limit} numbers");
+    const NUM_ATTEMPTS: usize = 5;
 
-    let table = sieve_for_smoothies(n, &factor_base, |_, _| sieving_limit).unwrap();
+    let mut cont = None;
 
-    println!("done collecting, building solution");
+    let mut table = vec![];
 
-    #[cfg(feature = "verbose")]
-    println!("{:?}", table);
+    for _ in 0..NUM_ATTEMPTS {
+        let sieving_limit = usize::max(
+            (factor_base.len() as f64 * ratio).ceil() as usize,
+            factor_base.len() + 5,
+        );
 
-    #[cfg(feature = "verbose")]
-    for (i, item) in table.iter().enumerate() {
-        println!("{i} -> {}", item.number);
+        println!("need about {sieving_limit} numbers");
+
+        let (mut additional_table, new_cont) = test_division_sieve(
+            n,
+            &factor_base,
+            |_, _| sieving_limit.saturating_sub(table.len()),
+            cont,
+        )
+        .unwrap();
+
+        table.append(&mut additional_table);
+
+        cont = Some(new_cont);
+
+        println!("done collecting, building solution");
+
+        #[cfg(feature = "verbose")]
+        println!("{:?}", table);
+
+        #[cfg(feature = "verbose")]
+        for (i, item) in table.iter().enumerate() {
+            println!("{i} -> {}", item.number);
+        }
+
+        let row_labels = (0..table.len()).collect_vec();
+        let rows = table.iter().map(|row| row.divisors.clone()).collect_vec();
+
+        let system =
+            CongruenceSystem::with_labels(&rows, factor_base.clone(), row_labels).transpose();
+
+        #[cfg(feature = "verbose")]
+        println!("{}", system.print_as_dense());
+
+        #[cfg(feature = "verbose")]
+        println!("---------");
+
+        if cfg!(feature = "multistage") {
+            if let Some(answ) = gaussian_multistage(n, table.clone(), system) {
+                return Some(answ);
+            }
+        } else if let Some(answ) = pivot_search(n, table.clone(), system) {
+            return Some(answ);
+        }
+
+        if factor_base.len() < 50 {
+            ratio *= 1.5;
+        } else {
+            ratio += 0.05;
+        }
+
+        println!("increasing number of smoothies to find")
     }
-
-    let row_labels = (0..table.len()).collect_vec();
-    let rows = table.iter().map(|row| row.divisors.clone()).collect_vec();
-
-    let system = CongruenceSystem::with_labels(&rows, factor_base, row_labels).transpose();
-
-    #[cfg(feature = "verbose")]
-    println!("{}", system.print_as_dense());
-
-    #[cfg(feature = "verbose")]
-    println!("---------");
-
-    if cfg!(feature = "multistage") {
-        gaussian_multistage(n, table, system)
-    } else {
-        pivot_search(n, table, system)
-    }
+    None
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
--- src/sieve.rs
+++ src/sieve.rs
@@ -1,5 +1,7 @@
-use std::{error::Error, time::Instant};
+use std::{error::Error, iter::repeat_with, time::Instant};
 
+use crypto_bigint::Zero;
+use itertools::Itertools;
 use num_bigint::BigUint;
 use num_traits::{Pow, ToPrimitive};
 
@@ -35,12 +37,13 @@ fn approximate_relations(_n: &NumberType, primes: &[usize]) -> usize {
 
 pub type SmoothiesVec = Vec<SmoothNumber<NumberType>>;
 
-pub fn sieve_for_smoothies<F: FnOnce(&NumberType, &[usize]) -> usize>(
+pub fn test_division_sieve<F: FnOnce(&NumberType, &[usize]) -> usize>(
     n: &NumberType,
     prime_table: &[usize],
     stop_criteria: F,
-) -> Result<SmoothiesVec, Box<dyn Error>> {
-    let lower_bound = n.sqrt().add_usize(1);
+    continuation: Option<NumberType>,
+) -> Result<(SmoothiesVec, NumberType), Box<dyn Error>> {
+    let lower_bound = continuation.unwrap_or_else(|| n.sqrt().add_usize(1));
 
     let mut result = vec![];
 
@@ -82,5 +85,142 @@ pub fn sieve_for_smoothies<F: FnOnce(&NumberType, &[usize]) -> usize>(
         next_number = next_number.add_usize(1);
     }
 
+    Ok((result, next_number.wrapping_add(NumberType::one())))
+}
+
+const BLOCK_SIZE: usize = 5000;
+
+pub fn block_division_sieve<F: FnOnce(&NumberType, &[usize]) -> usize>(
+    n: &NumberType,
+    prime_table: &[usize],
+    stop_criteria: F,
+) -> Result<SmoothiesVec, Box<dyn Error>> {
+    let lower_bound = n.sqrt().add_usize(1);
+
+    let mut result = vec![];
+
+    let mut next_number = lower_bound;
+
+    let total_numbers = stop_criteria(n, prime_table);
+
+    let mut numbers_to_find = total_numbers as isize;
+
+    let mut current_bound = lower_bound;
+
+    let step = NumberType::convert_usize(BLOCK_SIZE);
+
+    let mut last_time = Instant::now();
+
+    while numbers_to_find > 0 {
+        let mut produced_items = search_block(n, BLOCK_SIZE, current_bound, prime_table);
+
+        #[cfg(feature = "verbose")]
+        println!("block produced {} items", produced_items.len());
+
+        current_bound = current_bound.wrapping_add(&step);
+
+        numbers_to_find -= produced_items.len() as isize;
+
+        result.append(&mut produced_items);
+
+        let now = Instant::now();
+
+        if (now - last_time).as_secs() >= 5 {
+            last_time = now;
+            println!(
+                "done {:.1}%",
+                (total_numbers as isize - numbers_to_find) as f64 / total_numbers as f64 * 100f64
+            );
+        }
+    }
     Ok(result)
+}
+
+struct BlockEntry {
+    original_number: NumberType,
+    accumulator: NumberType,
+    factorization: Vec<(usize, usize)>,
+}
+
+fn search_block(
+    n: &NumberType,
+    block_size: usize,
+    mut start: NumberType,
+    prime_table: &[usize],
+) -> Vec<SmoothNumber<NumberType>> {
+    assert!(block_size > prime_table.last().cloned().unwrap());
+    // #[cfg(feature = "verbose")]
+    println!(
+        "working with block size {} starting at {}",
+        block_size,
+        start.to_varsize()
+    );
+
+    let mut block = repeat_with(|| {
+        let number = start.clone();
+        start = start.wrapping_add(NumberType::one());
+
+        BlockEntry {
+            original_number: number,
+            accumulator: number.modpow2(n),
+            factorization: Vec::with_capacity(16),
+        }
+    })
+    .take(block_size)
+    .collect_vec();
+
+    'primeiter: for &prime in prime_table {
+        //find start of sequence by trying different items
+        let mut idx = 0;
+
+        loop {
+            unimplemented!("this search is wrong, use tonelli-shanks to find roots");
+            let (d, r) = block[idx].accumulator.divmod(prime);
+            if r.is_zero().unwrap_u8() == 1 {
+                break;
+            }
+            idx += 1;
+            if idx >= block.len() {
+                continue 'primeiter;
+            }
+        }
+
+        println!("prime is {prime}, idx is {idx}");
+
+        while idx < block.len() {
+            dbg!(block[idx].accumulator.rem_short(prime).to_varsize());
+            dbg!(idx);
+            let mut exponent = 0;
+            loop {
+                let (d, r) = block[idx].accumulator.divmod(prime);
+                if r != NumberType::from_u32(0) {
+                    break;
+                }
+                exponent += 1;
+                block[idx].accumulator = d;
+            }
+
+            dbg!(exponent);
+
+            block[idx].factorization.push((prime, exponent));
+            idx += prime;
+        }
+    }
+
+    block
+        .into_iter()
+        .filter_map(|item| {
+            if &item.accumulator != NumberType::one() {
+                return None;
+            }
+
+            #[cfg(feature = "verbose")]
+            println!("found number {}", item.original_number.to_varsize());
+
+            Some(SmoothNumber {
+                number: item.original_number,
+                divisors: item.factorization,
+            })
+        })
+        .collect_vec()
 }
